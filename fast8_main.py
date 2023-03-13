3a41637ef00d8c4517420963ce3a2a1d3ebf3f1b
fast8saim <127437230+fast8saim@users.noreply.github.com>
2023-03-13 11:34:28 +0500

    Start adb server

--- fast8_main.py
+++ fast8_main.py
@@ -1,1 +1,30 @@
-print('Android hello')
+import subprocess
+import tkinter
+from tkinter import ttk
+
+
+def run_command(exec_command):
+    process = subprocess.Popen(exec_command)
+    process.wait()
+
+
+def create_button(app_form, button_text, button_column, button_row, button_width, button_command):
+    button = ttk.Button(app_form)
+    button['width'] = button_width
+    button['text'] = button_text
+    button['command'] = button_command
+    button.grid(column=button_column, row=button_row)
+
+
+def start_adb_service():
+    run_command(f'adb start-server')
+
+
+root = tkinter.Tk()
+root.title("FAST8 android ADB manager ")
+form = ttk.Frame(root, padding=5)
+form.grid()
+
+create_button(form, "Запустить службу ABD", 1, 1, 30, start_adb_service)
+
+root.mainloop()
